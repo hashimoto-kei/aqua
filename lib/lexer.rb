979ddf4cf5048ac26f22cb69624a257605412bdc
hashimoto-kei <hashimoto.keigo88@gmail.com>
2026-05-11 06:46:56 +0900

    Refactoring

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -83,7 +83,7 @@ class Lexer
         advance
       else
         @input.ungetc(c)
-        @token = {type: :'/', value: nil}
+        @token = {type: :/, value: nil}
       end
     in /\d/
       @input.ungetc(c)
