57041f7f6b9e36b618e3977d0126253a6730d114
hashimoto-kei <hashimoto.keigo88@gmail.com>
2026-05-11 20:22:17 +0900

    Add color to message for test result

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+RED='\033[031m'
+GREEN='\033[032m'
+RESET='\033[0m'
+
 for x in test/script/*.aq
 do
     name=$(basename "$x" .aq)
     diff -u "test/expected/$name.txt" <(./aqua $x)
-    [ $? == 0 ] && echo "pass: $x" || echo "fail: $x"
+    [ $? == 0 ] && printf "${GREEN}pass${RESET}: $x\n" || printf "${RED}fail${RESET}: $x\n"
 done
