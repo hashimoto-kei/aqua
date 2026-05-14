3ae9afa8be5deaaf1653554b40fec668052ea5a1
hashimoto-kei <hashimoto.keigo88@gmail.com>
2026-05-14 14:51:00 +0900

    Refactoring

--- lib/nodes/node_func_call.rb
+++ lib/nodes/node_func_call.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class NodeFuncCall
-  def initialize(func, args)
-    @func = func
+  def initialize(func_name, args)
+    @func_name = func_name
     @args = args
   end
 
   def eval
-    func = SymbolTable.get(@func)
+    func = SymbolTable.get(@func_name)
     if func.nil?
-      raise "Runtime Error. undifined function: #{@func.to_sym}"
+      raise "Runtime Error. undifined function: #{@func_name}"
     end
     args = @args.map(&:eval)
     func.call(args)
